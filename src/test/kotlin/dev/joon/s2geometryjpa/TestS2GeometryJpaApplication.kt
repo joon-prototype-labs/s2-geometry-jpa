214cdbddc57f28377741d4f98b72c71ba04bcc9e
sijun-yang <yangsijun@bondit.io>
2024-09-10 13:18:26 +0900

    Delete TestS2GeometryJpaApplication.kt

--- src/test/kotlin/dev/joon/s2geometryjpa/TestS2GeometryJpaApplication.kt
+++
@@ -1,9 +0,0 @@
-package dev.joon.s2geometryjpa
-
-import org.springframework.boot.fromApplication
-import org.springframework.boot.with
-
-
-fun main(args: Array<String>) {
-    fromApplication<S2GeometryJpaApplication>().with(TestcontainersConfiguration::class).run(*args)
-}
